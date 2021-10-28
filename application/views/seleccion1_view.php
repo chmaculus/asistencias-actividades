beeef573c361cefb82acb83db8e757ccc7aab8b0
Christian Máculus <chmaculus@gmail.com>
2021-10-28 18:02:37 -0300

    Changes to be committed: 	new file:   application/controllers/Seleccion1.php 	new file:   application/views/seleccion1_view.php 	new file:   assets/css/seleccion1.css

---
+++ application/views/seleccion1_view.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta content="text/html; charset=iso-8859-1" http-equiv="content-type" />
+</head>
+
+<!-- Core CSS de Bootstrap-->
+<link href="<?php  echo base_url().bootstrap_path;?>css/bootstrap.css" rel="stylesheet">
+
+<link href="<?php  echo base_url();?>assets/css/seleccion1.css" rel="stylesheet">
+
+<style>
+	.bd-placeholder-img {
+		font-size: 1.125rem;
+		text-anchor: middle;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		user-select: none;
+	}
+
+	@media (min-width: 768px) {
+		.bd-placeholder-img-lg {
+			font-size: 3.5rem;
+		}
+	}
+</style>
+
+<center>
+
+<br><table><tr>
+<td><img src="<?php echo base_url()."assets/images/club_house.png";?>"></td>
+
+<?php /*
+ width="5" height="5"
+*/
+?>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+<td><label class="label1"></label></td>
+</tr></table>
+
+<br>
+<table>
+	<tr><td><button class="btn-actividades" type="button">ACTIVIDADES</button></td></tr>
+	<tr><td><br></td></tr>
+	<tr><td><button class="btn-clases" type="button">CLASES</button></td></tr>
+	<tr><td><br></td></tr>
+	<tr><td><button class="btn-espacios" type="button">ESPACIOS</button></td></tr>
+</table>
+
+
+
+
+
+
+
+
+
+</center>
