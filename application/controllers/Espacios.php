54b2597184689b5bfb54044e9d653737674a98b0
Christian Máculus <chmaculus@gmail.com>
2021-10-28 16:46:44 -0300

    Changes to be committed: 	new file:   application/controllers/Espacios.php 	new file:   application/models/Espacios_model.php 	new file:   application/views/espacios_view.php 	modified:   assets/css/clases.css 	new file:   assets/css/espacios.css

---
+++ application/controllers/Espacios.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Espacios extends CI_Controller {
+
+	 public function __construct()
+	 {
+			 parent::__construct();
+			 $this->load->model('Espacios_model');
+	 }
+
+
+	public function index()
+	{
+		$data = $this->Espacios_model->getespacios();
+		$this->load->view('Espacios_view', array(
+		'data' =>$data
+		));
+		//echo json_encode($data);
+	}
+}
+
