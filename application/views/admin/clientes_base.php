e819b45130482d65d950f87219eb503ed20f3e9f
Christian Máculus <chmaculus@gmail.com>
2021-11-16 15:51:05 -0300

    Changes to be committed: 	modified:   application/views/admin/clientes_base.php

--- application/views/admin/clientes_base.php
+++ application/views/admin/clientes_base.php
@@ -1,35 +1,23 @@
 <!DOCTYPE html>
 <html lang="esp">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Login Club House</title>
-	<link href="<?php echo base_url(); ?>assets/css/clientes_ingreso.css" rel="stylesheet">
-	<script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Club House</title>
+    <link href="<?php echo base_url(); ?>assets/css/clientes_ingreso.css" rel="stylesheet">
+    <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
 </head>
 
 
 <center>
-
-<form action="<?php echo "";?>" method="post">
-	<table border="1">
-		<tr>
-			<td></td><td><input type="text" name="manzana"></td>
-			<td></td><td><input type="text" name="casa"></td>
-		</tr>
-		</table>
-		<input type="submit" name="ACEPTAR" value="ACEPTAR">
-	</form>
-<?php
-if(!$data){
-	exit;
-}
-
-include("clientes_listado.inc.php");
-
-
-?>
-
-
-
+<titulo>clientes_dalvian</titulo>
+<table>
+	<tr>
+		<td><A href="clientes_dalvian_modifica.php"><button>Ingresar</button></a></td>
+		<td><A href="clientes_dalvian_busqueda.php"><button>Buscar</button></a></td>
+		<td><A href="<?php base_url();?>busqueda_dossa_main"><button>Buscar DOSSA</button></a></td>
+		<td><A href="listado"><button>Listado</button></a></td>
+	</tr>
+</table>
+<br><br>
