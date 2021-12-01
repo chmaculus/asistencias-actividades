358f76d84d797e0c2bec3efd35174eb7b4263df5
Christian Máculus <chmaculus@gmail.com>
2021-12-01 13:08:53 -0300

    Changes to be committed: 	new file:   admin/clientes_asistencia_listado.php 	modified:   includes/funciones.php

---
+++ admin/clientes_asistencia_listado.php
@@ -0,0 +1,117 @@
+<?php
+include("index.php");
+include("../includes/connect.php");
+include("../includes/funciones.php");
+
+
+
+$num_dias_mes = date("t",mktime(0,0,0,$mes,1,$year));
+
+
+?>
+<form action="<?php echo $SCRIPT_NAME ?>" method="post" enctype="multipart/form-data">
+<table class="t1">
+	<tr>
+		<td>Fecha desde</td>
+		<td>Fecha hasta</td>
+		<td>Residente</td>
+		<td>Actividad</td>
+	</tr>
+	<tr>
+		<td><input type="text" name="fecha_desde" value="<?php echo date("01/m/Y");?>" size="10"></td>
+		<td><input type="text" name="fecha_hasta" value="<?php echo $num_dias_mes.date("/m/Y");?>" size="10"></td>
+		<td><input type="text" name="residente" value="a" size="10"></td>
+		<td>
+			<select name="actividades">
+				<?php
+				$q='select * from actividades where activado=1 and mostrar_asistencias=1 group by actividad order by actividad';
+				$res=mysql_query($q);
+				echo '<option value="" label="Todas"></option>'.chr(10);
+				while($row=mysql_fetch_array($res)){
+					if($_POST["actividades"]==$row["Actividad"]){
+						echo '<option value="'.$row["Actividad"].'" label="'.$row["Actividad"].'" selected></option>'.chr(10);
+					}else{
+						echo '<option value="'.$row["Actividad"].'" label="'.$row["Actividad"].'"></option>'.chr(10);
+					}
+				}
+				
+				?>
+			</select>
+
+		</td>
+	</tr>
+</table>
+
+Ordernar por:
+<table border="1">
+	<tr>
+		<td>Apellido</td><td><input type="radio" name="ordenar" value="apellido" checked></td>
+		</tr><tr>
+		<td>Fecha/hora</td><td><input type="radio" name="ordenar" value="fecha"></td>
+		</tr><tr>
+		<td>Detalle</td><td><input type="radio" name="ordenar" value="detalle"></td>
+	</tr>
+</table>
+<input type="submit" name="ACEPTAR" value="ACEPTAR">
+
+</form>
+
+
+
+<?php
+
+if(!$_POST["fecha_desde"]){
+	exit;
+}
+
+$query='select * from clientes_asistencias 
+where fecha_ingreso>="'.fecha_conv("/",$_POST["fecha_desde"]).'" 
+	and  fecha_ingreso<="'.fecha_conv("/",$_POST["fecha_hasta"]).'" 
+	';
+
+if($_POST["orden"]=="fecha"){
+	$order=' order by fecha_ingreso, hora_ingreso ';
+}
+
+
+
+
+
+//$query='select * from clientes_asistencias where fecha>=';
+
+
+
+echo $query.";<br>";
+
+$result=mysql_query($query);
+if(mysql_error()){echo mysql_error()."<br>".$query."<br>";}
+
+
+echo '<table border="1">';
+echo "<tr>";
+	echo "<th>Codigo residente</th>";
+	echo "<th>codigo facturacion</th>";
+	echo "<th>Apellido</th>";
+	echo "<th>Nombres</th>";
+	echo "<th>Numero_documento</th>";
+	echo "<th>Tipo</th>";
+	echo "<th>Detalle</th>";
+	echo "<th>Fecha ingreso</th>";
+	echo "<th>Hora ingreso</th>";
+echo "</tr>";
+
+while($row=mysql_fetch_array($result)){
+	echo "<tr>";
+	echo '<td>'.$row["id_cliente"].'</td>';
+	echo '<td>'.$row["codigo_facturacion"].'</td>';
+	echo '<td>'.$row["apellido"].'</td>';
+	echo '<td>'.$row["nombres"].'</td>';
+	echo '<td>'.$row["numero_documento"].'</td>';
+	echo '<td>'.$row["tipo"].'</td>';
+	echo '<td>'.$row["detalle"].'</td>';
+	echo '<td>'.fecha_conv("-",$row["fecha_ingreso"]).'</td>';
+	echo '<td>'.$row["hora_ingreso"].'</td>';
+	echo "</tr>".chr(10);
+}
+?>
+</table></center>
--- includes/funciones.php
+++ includes/funciones.php
@@ -12,4 +12,20 @@ function file_write($file_name, $data) {
       fclose($file);
   }
 
+
+#---------------------------------------------------------------------------------------------
+function fecha_conv($separador, $fecha){
+      $f=explode($separador, $fecha);
+
+      if($separador=="/"){
+            $fecha=$f[2]."-".$f[1]."-".$f[0];
+      }
+
+      if($separador=="-"){
+            $fecha=$f[2]."/".$f[1]."/".$f[0];
+      }
+return $fecha;
+}
+#---------------------------------------------------------------------------------------------
+
 ?>
