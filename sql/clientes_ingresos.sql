8b097e16f2e117edd1e729485e63fec8e1a45d65
Christian Máculus <chmaculus@gmail.com>
2021-12-01 10:18:30 -0300

    Changes to be committed: 	modified:   sql/clientes_ingresos.sql

--- sql/clientes_ingresos.sql
+++ sql/clientes_ingresos.sql
@@ -9,9 +9,15 @@ CREATE TABLE `clientes_asistencias` (
   `apellido` varchar(35) DEFAULT NULL,
   `nombres` varchar(35) DEFAULT NULL,
   `numero_documento` int(13) DEFAULT NULL,
+  `tipo` int(2) DEFAULT NULL,
+  `detalle` varchar(25),
   fecha_ingreso date,
   hora_ingreso time,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=242 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+alter table clientes_asistencias add index codigo_residente(codigo_residente);
+alter table clientes_asistencias add index codigo_facturacion(codigo_facturacion);
+alter table clientes_asistencias add index tipo(tipo);
+alter table clientes_asistencias add index detalle(detalle);
