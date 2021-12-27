b26b2ccf40471026773f54fca700d9fb4d27e85c
Christian Maculus <cmaculus@focasoftware.com>
2021-12-27 12:48:35 -0300

    Changes to be committed: 	modified:   admin/header.inc.php 	modified:   admin/index.php 	modified:   assets/css/prueba1.css

--- admin/header.inc.php
+++ admin/header.inc.php
@@ -5,7 +5,7 @@
                 <td class="botones_navegacion"><a href="clientes_ingreso.php"><button><img src="../assets/images/ingreso.svg" alt="puertita"></button></a></td>
                 <td class="botones_navegacion"><a href="clientes_busqueda.php"><button><img src="../assets/images/buscar.svg" alt="lupita"></button></a></td>
                 <td class="botones_navegacion"><a href="dossa_main.php"><button><img src="../assets/images/libro.svg" alt="librito"></button></a></td>
-                <td class="botones_navegacion"><a href="clientes_asistencia_listado.php"><button><img src="../assets/images/listado.svg" alt="librito"></button></a></td>
+                <td class="botones_navegacion"><a href="clientes_asistencia_listado.php"><button><img src="../assets/images/listado.svg" alt="listita"></button></a></td>
             </tr>
             <tr>
                 <td class="botones_navegacion">Ingresar</td>
